4a420e590891efc24fb5f90e7cc5e7e0c9e98362
mariesonko <mariesonko62@gmail.com>
2018-04-25 23:14:57 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  attr_accessor :name, :job
+
+  def initialize
+    @name = name
+    @job = job
+  end
+
+  def name= (name)
+    @name=name
+  end
+
+  def job= (job)
+    @job = job 
+  end
+end
